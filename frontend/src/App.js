f0ab397cfc09580230fdaafaf595f31c6a395272
armank57 <112572260+armank57@users.noreply.github.com>
2024-02-18 16:40:24 -0500

    Created UI for log-in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import StudyPage from './StudyPage';
+import LogIn from './LogIn';
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <LogIn />
+      </header>
     </div>
   );
 }
