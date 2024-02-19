70c0cefa36d0c224e8939ef1193df34837665b7c
armank57 <112572260+armank57@users.noreply.github.com>
2024-02-18 23:16:12 -0500

    Finished styling Log-In page and added Link for reset password

--- frontend/src/LogIn.js
+++ frontend/src/LogIn.js
@@ -1,6 +1,7 @@
-import { React, useState } from 'react';
 import './LogIn.css';
-import { Box, Button, Card, CardContent, Container, TextField } from '@mui/material';
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Button, Card, CardContent, Container, TextField, Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
@@ -37,78 +38,96 @@ function LogIn() {
     };
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container maxWidth="sm">
-                <Card style={{ backgroundColor: theme.palette.primary.main }}>
-                    <CardContent>
-                        <Box component="form" onSubmit={loginHandler}>
-                            <Grid container spacing={2}>
-                                <Grid xs={12}>
-                                    <TextField
-                                        label="Email"
-                                        type="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        placeholder="Enter Purdue Email Address"
-                                        variant="outlined"
-                                        margin="normal"
-                                        InputProps={{
-                                            style: { backgroundColor: 'white' },
-                                        }}
-                                        InputLabelProps={{
-                                            style: { color: 'black' },
-                                        }}
-                                        required
-                                        fullWidth
-                                    />
+        <div className="login">
+            <ThemeProvider theme={theme}>
+                <Container maxWidth="sm" style={{ 
+                    height: '100vh', 
+                    display: 'flex', 
+                    flexDirection: 'column',
+                    justifyContent: 'center', 
+                    alignItems: 'center',
+                }}>
+                    <Typography variant="h3" style={{ paddingBottom: '20px' }}>
+                        Log In
+                    </Typography>
+                    <Card style={{ backgroundColor: theme.palette.primary.main }}>
+                        <CardContent>
+                            <Box component="form" onSubmit={loginHandler}>
+                                <Grid container spacing={2}>
+                                    <Grid xs={12}>
+                                        <TextField
+                                            label="Email"
+                                            type="email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            placeholder="Enter Purdue Email Address"
+                                            variant="outlined"
+                                            margin="normal"
+                                            InputProps={{
+                                                style: { backgroundColor: 'white' },
+                                            }}
+                                            InputLabelProps={{
+                                                style: { color: 'black' },
+                                            }}
+                                            required
+                                            fullWidth
+                                        />
+                                    </Grid>
+                                    <Grid xs={12}>
+                                        <TextField
+                                            label="Password"
+                                            type="password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            placeholder="Enter Password"
+                                            variant="outlined"
+                                            margin="normal"
+                                            InputProps={{
+                                                style: { backgroundColor: 'white' },
+                                            }}
+                                            InputLabelProps={{
+                                                style: { color: 'black' },
+                                            }}
+                                            required
+                                            fullWidth
+                                        />
+                                    </Grid>
+                                    <Grid xs={12}>
+                                        <Typography align="right">
+                                            <Link to="reset-password">
+                                                Forgot Password?
+                                            </Link>
+                                        </Typography>
+                                    </Grid>
+                                    <Grid xs={12} sm={6}>
+                                        <Button
+                                            name="createAccount"
+                                            variant="contained"
+                                            color="secondary"
+                                            fullWidth
+                                            onClick={createAccountHandler}
+                                        >
+                                            Create Account
+                                        </Button>
+                                    </Grid>
+                                    <Grid xs={12} sm={6}>
+                                        <Button
+                                            name="logIn"
+                                            type="submit"
+                                            variant="contained"
+                                            color="secondary"
+                                            fullWidth
+                                        >
+                                            Log In
+                                        </Button>
+                                    </Grid>
                                 </Grid>
-                                <Grid xs={12}>
-                                    <TextField
-                                        label="Password"
-                                        type="password"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        placeholder="Enter Password"
-                                        variant="outlined"
-                                        margin="normal"
-                                        InputProps={{
-                                            style: { backgroundColor: 'white' },
-                                        }}
-                                        InputLabelProps={{
-                                            style: { color: 'black' },
-                                        }}
-                                        required
-                                        fullWidth
-                                    />
-                                </Grid>
-                                <Grid xs={12} sm={6}>
-                                    <Button
-                                        name="createAccount"
-                                        variant="contained"
-                                        color="secondary"
-                                        fullWidth
-                                        onClick={createAccountHandler}
-                                    >
-                                        Create Account
-                                    </Button>
-                                </Grid>
-                                <Grid xs={12} sm={6}>
-                                    <Button
-                                        name="logIn"
-                                        type="submit"
-                                        variant="contained"
-                                        color="secondary"
-                                        fullWidth
-                                    >
-                                        Log In
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                    </CardContent>
-                </Card>
-            </Container>
-        </ThemeProvider>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </Container>
+            </ThemeProvider>
+        </div>
     );
 }
 
