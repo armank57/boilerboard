85a90be056652712de2d637cc4787038e258dec3
armank57 <112572260+armank57@users.noreply.github.com>
2024-02-18 22:58:40 -0500

    Started laying out routers for navigation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,20 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import StudyPage from './StudyPage';
-import CreateQuiz from './CreateQuiz';
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import ResetPassword from './ResetPassword';
+
+
+/*
+  * The following examples are for routes:
+  * <Route path="/login" element={<LogIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+  */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-        <App />
-        <Routes>
-          <Route exact path="/" element={<StudyPage /> } />
-          <Route path="/create-quiz" element={<CreateQuiz />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+      <Routes>
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
