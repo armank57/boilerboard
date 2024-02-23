824d688f02eb1ff23eab784ce33dcdbf2d71816a
David <cox407@purdue.edu>
2024-02-23 11:35:43 -0500

    I implemented nearly all of the quiz display/taking quiz ui and creation of quizzes ui.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ResponsiveAppBar from './ResponsiveAppBar';
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Registration from './pages/Registration';
-import Login from './pages/Login';
-import ResetPassword from './pages/ResetPassword';
-import ResetPasswordConfirm from './pages/ResetPasswordConfirm';
-import ResetPasswordSentDialouge from './pages/ResetPasswordSentDialouge';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import StudyPage from './StudyPage';
+import CreateQuiz from './CreateQuiz';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      {/* <ResponsiveAppBar /> */}
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+        <Routes>
+          <Route exact path="/" element={<StudyPage /> } />
+          <Route path="/create-quiz" element={<CreateQuiz />} />
+        </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
