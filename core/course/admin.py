4acae3b8b3f5ac9f303f2cc618f35e6aa8dbe68b
Soham Sharma <sharm710@purdue.edu>
2024-03-28 18:03:26 -0400

    Add functionality to see/add/remove users to CourseAdmin

--- core/course/admin.py
+++ core/course/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from core.course.models import Course
+from core.user.models import User
+
+class UserInline(admin.TabularInline):
+    model = User.joined_courses.through
+    extra = 1
 
 # Register your models here.
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin): 
     list_display = ("name", "public_id")
+    inlines = [UserInline]
