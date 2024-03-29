bc6046f2b526c17795edcc5bbaa761aad8433a1a
Soham Sharma <sharm710@purdue.edu>
2024-03-28 22:53:32 -0400

    Merged with main

--- BoilerBoard/urls.py
+++ BoilerBoard/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 
+admin.site.site_url = 'http://localhost:3000/' 
 admin.site.site_header = 'BoilerBoard Administration'
 
 urlpatterns = [
